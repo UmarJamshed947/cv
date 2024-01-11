3cc1164a5be688460a218d25d539486222447a5a
Umar Jamshed <128473349+UmarJamshed947@users.noreply.github.com>
2024-01-11 18:35:36 +0500

    new screen added

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,9 +1,12 @@
 import 'package:cv/widgets/Buttons/gradient_button.dart';
 import 'package:cv/widgets/cursor/animated_circle_cursor.dart';
+import 'package:cv/widgets/shapes/animated_shape_container.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+import '../widgets/Buttons/haptic_circle.dart';
 import '../widgets/Icons/padded_icons.dart';
+import '../widgets/Text_folder/poppins.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -12,50 +15,148 @@ class HomePage extends StatelessWidget {
       backgroundColor: Color(0xffF1FAFF),
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 250, vertical: 30),
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                AnimatedCircleCursorMouseRegion(
-                  child: SizedBox(
-                    height: 80,
-                    width: 80,
-                    child: Image.asset("assets/images/personal.png"),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AnimatedCircleCursorMouseRegion(
+                    child: SizedBox(
+                      height: 60,
+                      width: 60,
+                      child: Image.asset("assets/images/personal.png"),
+                    ),
                   ),
-                ),
-                Row(
-                  children: [
-                    PaddedIcons(
-                      ficon: FontAwesomeIcons.facebookF,
-                      color: Color(0xff03A9F4),
+                  Row(
+                    children: [
+                      PaddedIcons(
+                        ficon: FontAwesomeIcons.facebookF,
+                        color: Color(0xff03A9F4),
+                      ),
+                      PaddedIcons(
+                        ficon: FontAwesomeIcons.twitter,
+                        color: Color(0xff03A9F4),
+                      ),
+                      PaddedIcons(
+                        ficon: FontAwesomeIcons.youtube,
+                        color: Color(0xffFF0000),
+                      ),
+                      PaddedIcons(
+                        ficon: FontAwesomeIcons.instagram,
+                        color: Color(0xfff44535),
+                      ),
+                      SizedBox(width: 20),
+                      GradientButtonContainer(
+                          title: "Download CV",
+                          width: 250,
+                          height: 80,
+                          clr: [Color(0xffFF4518), Color(0xffFF4518)],
+                          overlayColor: Colors.red,
+                          isGradientVertical: false,
+                          onpressed: () {})
+                    ],
+                  )
+                ],
+              ),
+              SizedBox(height: 60),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      height: 500,
+                      child: Column(
+                        children: [
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Poppins(
+                              text: 'I am',
+                              color: Color(0xffFF4518),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Poppins(
+                              text: ' Umar Jamshed',
+                              color: Color(0xff222222),
+                              fontSize: 25,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: Poppins(
+                              text: ' A passionate Manager IT',
+                              color: Color(0xff888888),
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          SizedBox(height: 30),
+                          Align(
+                            alignment: Alignment.centerLeft,
+                            child: AnimatedCircleCursorMouseRegion(
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: HapticCircle(),
+                                  ),
+                                  SizedBox(width: 30),
+                                  Poppins(
+                                    text: "Play Video",
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                    PaddedIcons(
-                      ficon: FontAwesomeIcons.twitter,
-                      color: Color(0xff03A9F4),
-                    ),
-                    PaddedIcons(
-                      ficon: FontAwesomeIcons.youtube,
-                      color: Color(0xffFF0000),
-                    ),
-                    PaddedIcons(
-                      ficon: FontAwesomeIcons.instagram,
-                      color: Color(0xfff44535),
-                    ),
-                    SizedBox(width: 20),
-                    GradientButtonContainer(
-                        title: "Download CV",
-                        width: 250,
-                        height: 80,
-                        clr: [Color(0xffFF4518),Color(0xffFF4518)],
-                        overlayColor: Colors.red,
-                        isGradientVertical: false,
-                        onpressed: (){})
-                  ],
-                )
-              ],
-            ),
-          ],
+                  ),
+                  Expanded(
+                      flex: 1,
+                      child: Stack(
+                        children: [
+                         Positioned(
+                             top:30,
+                          left:100,
+                             child: AnimatedShapeContainer()),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                SizedBox(height: 40),
+                                SizedBox(
+                                  height: 500,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(30),
+                                    ),
+                                    child: Image.asset(
+                                      "assets/images/img.jpg",
+                                      fit: BoxFit.fill,
+                                    ),
+                                  ),
+                                ),
+
+
+                              ],
+                            ),
+                          ),
+                        ],
+                      ))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/widgets/Buttons/haptic_circle.dart
@@ -0,0 +1,46 @@
+import 'package:cv/widgets/cursor/animated_circle_cursor.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HapticCircle extends StatefulWidget {
+  @override
+  State<HapticCircle> createState() => _HapticCircleState();
+}
+
+class _HapticCircleState extends State<HapticCircle> {
+  double currentRadius = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (event){
+        setState(() {
+          currentRadius = 10;
+        });
+      },
+      onExit: (event){
+        setState(() {
+          currentRadius = 0;
+        });
+      },
+      child: AnimatedContainer(
+        duration: Duration(milliseconds: 100),
+        height: 40,
+        width: 40,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Color(0xffFF8324),
+          boxShadow: [
+            BoxShadow(color: Colors.grey.withOpacity(0.2), spreadRadius: 5)
+          ],
+        ),
+        child: Padding(
+          padding: EdgeInsets.only(left: 0.5),
+          child: Icon(FontAwesomeIcons.play,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
